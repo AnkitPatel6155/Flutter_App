3076998e4e2a3f2684f9b2f6dbcd06653d51c589
Raj koshta <rajkoshta2000@gmail.com>
2025-06-29 20:30:06 +0530

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,28 @@ void main() {
           title: Text('My First App Ankit', selectionColor: Colors.white),
           backgroundColor: Colors.deepPurpleAccent,
         ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-              'https://cdn.prod.website-files.com/613b7b21cfaac7287728a106/62f12194232303b143639bdd_DeepAR%20Flutter%20blog-p-1080.jpg',
+        // use column widget for two images
+        body: Column(
+          children: [
+            ClipRRect(
+              //give radius to first image
+              borderRadius: BorderRadius.circular(500),
+              //this is the first image "Network Image"
+              child: Image(
+                image: NetworkImage(
+                  'https://cdn.prod.website-files.com/613b7b21cfaac7287728a106/62f12194232303b143639bdd_DeepAR%20Flutter%20blog-p-1080.jpg',
+                ),
+              ),
             ),
-            height: 500,
-            width: 500,
-          ),
+            //this is the second image
+            Material(
+              elevation: 8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.asset('images/google.png'),
+              ),
+            ),
+          ],
         ),
       ),
     ),
